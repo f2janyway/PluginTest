8d82bdc22a78c9007bec6bdb467045cd773d8aa2
GitHub Action <action@github.com>
2022-02-12 05:01:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/f2janyway/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/f2janyway/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.f2janyway.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.f2janyway.plugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/f2janyway/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/f2janyway/plugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.f2janyway.plugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.f2janyway.plugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
